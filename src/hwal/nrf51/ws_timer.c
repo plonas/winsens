db4f309913ba5e9044bd41d00fbe9d8ca80e39f5
plonas <damian.plonek@gmail.com>
2020-02-23 15:04:39 +0100

    Added handling timer callback in a sheduler task.

--- src/hwal/nrf51/ws_timer.c
+++ src/hwal/nrf51/ws_timer.c
@@ -6,6 +6,9 @@
  */
 
 #include "ws_timer.h"
+#include "ws_task_queue.h"
+#define WS_LOG_MODULE_NAME "TIMR"
+#include "ws_log.h"
 
 #include "nrf_drv_timer.h"
 
@@ -20,27 +23,33 @@ typedef struct
 } WS_Timer_t;
 
 static void WS_TimerCallback(
+    void *p_data,
+    uint16_t data_size);
+static void WS_TimerIrqHandler(
     nrf_timer_event_t eventType,
     void* context);
 
+static uint32_t counter = 0;
 static const nrf_drv_timer_t ws_systemTimer = NRF_DRV_TIMER_INSTANCE(1);
 static WS_Timer_t ws_timers[WS_TIMER_TIMERS_NUM] = { 0, NULL };
 
 WINSENS_Status_e WS_TimerInit(void)
 {
-    // init a timer
-    nrf_drv_timer_config_t timer_cfg = NRF_DRV_TIMER_DEFAULT_CONFIG;
-    timer_cfg.frequency = NRF_TIMER_FREQ_31250Hz;
-    uint32_t err_code = nrf_drv_timer_init(&ws_systemTimer, &timer_cfg, WS_TimerCallback);
-    APP_ERROR_CHECK(err_code);
-    nrf_drv_timer_extended_compare(&ws_systemTimer, NRF_TIMER_CC_CHANNEL0, 3125UL, NRF_TIMER_SHORT_COMPARE0_CLEAR_MASK, false);
-    nrf_drv_timer_enable(&ws_systemTimer);
-
     uint32_t i;
     for (i = 0; i < WS_TIMER_TIMERS_NUM; ++i)
     {
         ws_timers[i] = (WS_Timer_t) { 0, NULL };
     }
+
+    // init a timer
+    nrf_drv_timer_config_t timer_cfg = NRF_DRV_TIMER_DEFAULT_CONFIG;
+    timer_cfg.frequency = NRF_TIMER_FREQ_31250Hz;
+    uint32_t err_code = nrf_drv_timer_init(&ws_systemTimer, &timer_cfg, WS_TimerIrqHandler);
+    APP_ERROR_CHECK(err_code);
+    nrf_drv_timer_extended_compare(&ws_systemTimer, NRF_TIMER_CC_CHANNEL0, 3125UL, NRF_TIMER_SHORT_COMPARE0_CLEAR_MASK, true);
+    nrf_drv_timer_enable(&ws_systemTimer);
+
+    return WINSENS_OK;
 }
 
 void WS_TimerDeinit(void)
@@ -89,24 +98,29 @@ const void* WS_TimerGetNativeTimer(void)
 }
 
 static void WS_TimerCallback(
+    void *p_data,
+    uint16_t data_size)
+{
+    uint32_t i;
+    for (i = 0; i < WS_TIMER_TIMERS_NUM; ++i)
+    {
+        if (ws_timers[i].callback
+            && 0 == (counter % ws_timers[i].interval))
+        {
+            ws_timers[i].callback(i);
+        }
+    }
+}
+
+static void WS_TimerIrqHandler(
     nrf_timer_event_t eventType,
     void* context)
 {
-    static uint32_t counter = 0;
-
     switch (eventType)
     {
         case NRF_TIMER_EVENT_COMPARE0:
             ++counter;
-            uint32_t i;
-            for (i = 0; i < WS_TIMER_TIMERS_NUM; ++i)
-            {
-                if (ws_timers[i].callback
-                    && 0 == (counter % ws_timers[i].interval))
-                {
-                    ws_timers[i].callback(i);
-                }
-            }
+            WS_TaskQueueAdd(NULL, 0, WS_TimerCallback);
             break;
 
         default:
