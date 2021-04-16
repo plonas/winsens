2626b70c1e4848dd0059b3e2db5dbacf332ee7a9
plonas <damian.plonek@gmail.com>
2021-04-16 12:32:27 +0200

    Fix logging macros.

--- src/os/log/log.h
+++ src/os/log/log.h
@@ -25,15 +25,15 @@
 #define LOG_INFO_CHECK(s)               LOG_IF_INFO(WINSENS_OK != (s), #s "(%u) != WINSENS_OK | %s:%u", s, __FUNCTION__, __LINE__)
 #define LOG_DEBUG_CHECK(s)              LOG_IF_DEBUG(WINSENS_OK != (s), #s "(%u) != WINSENS_OK | %s:%u", s, __FUNCTION__, __LINE__)
 
-#define LOG_ERROR_RETURN(s, r)          do{ if (WINSENS_OK != s) { LOG_ERROR_INTERNAL(#s "(%u) != WINSENS_OK | %s:%u", s, __FUNCTION__, __LINE__); return r; } }while(0)
-#define LOG_WARNING_RETURN(s, r)        do{ if (WINSENS_OK != s) { LOG_WARNING_INTERNAL(#s "(%u) != WINSENS_OK | %s:%u", s, __FUNCTION__, __LINE__); return r; } }while(0)
-#define LOG_INFO_RETURN(s, r)           do{ if (WINSENS_OK != s) { LOG_INFO_INTERNAL(#s "(%u) != WINSENS_OK | %s:%u", s, __FUNCTION__, __LINE__); return r; } }while(0)
-#define LOG_DEBUG_RETURN(s, r)          do{ if (WINSENS_OK != s) { LOG_DEBUG_INTERNAL(#s "(%u) != WINSENS_OK | %s:%u", s, __FUNCTION__, __LINE__); return r; } }while(0)
+#define LOG_ERROR_RETURN(s, r)          do{ if (WINSENS_OK != (s)) { LOG_ERROR_INTERNAL(#s "(%u) != WINSENS_OK | %s:%u", s, __FUNCTION__, __LINE__); return r; } }while(0)
+#define LOG_WARNING_RETURN(s, r)        do{ if (WINSENS_OK != (s)) { LOG_WARNING_INTERNAL(#s "(%u) != WINSENS_OK | %s:%u", s, __FUNCTION__, __LINE__); return r; } }while(0)
+#define LOG_INFO_RETURN(s, r)           do{ if (WINSENS_OK != (s)) { LOG_INFO_INTERNAL(#s "(%u) != WINSENS_OK | %s:%u", s, __FUNCTION__, __LINE__); return r; } }while(0)
+#define LOG_DEBUG_RETURN(s, r)          do{ if (WINSENS_OK != (s)) { LOG_DEBUG_INTERNAL(#s "(%u) != WINSENS_OK | %s:%u", s, __FUNCTION__, __LINE__); return r; } }while(0)
 
-#define LOG_ERROR_BOOL_RETURN(s, r)     do{ if (true != s) { LOG_ERROR_INTERNAL(#s " != true | %s:%u", s, __FUNCTION__, __LINE__); return r; } }while(0)
-#define LOG_WARNING_BOOL_RETURN(s, r)   do{ if (true != s) { LOG_WARNING_INTERNAL(#s " != true | %s:%u", s, __FUNCTION__, __LINE__); return r; } }while(0)
-#define LOG_INFO_BOOL_RETURN(s, r)      do{ if (true != s) { LOG_INFO_INTERNAL(#s " != true | %s:%u", s, __FUNCTION__, __LINE__); return r; } }while(0)
-#define LOG_DEBUG_BOOL_RETURN(s, r)     do{ if (true != s) { LOG_DEBUG_INTERNAL(#s " != true | %s:%u", s, __FUNCTION__, __LINE__); return r; } }while(0)
+#define LOG_ERROR_BOOL_RETURN(s, r)     do{ if (true != (s)) { LOG_ERROR_INTERNAL(#s " != true | %s:%u", s, __FUNCTION__, __LINE__); return r; } }while(0)
+#define LOG_WARNING_BOOL_RETURN(s, r)   do{ if (true != (s)) { LOG_WARNING_INTERNAL(#s " != true | %s:%u", s, __FUNCTION__, __LINE__); return r; } }while(0)
+#define LOG_INFO_BOOL_RETURN(s, r)      do{ if (true != (s)) { LOG_INFO_INTERNAL(#s " != true | %s:%u", s, __FUNCTION__, __LINE__); return r; } }while(0)
+#define LOG_DEBUG_BOOL_RETURN(s, r)     do{ if (true != (s)) { LOG_DEBUG_INTERNAL(#s " != true | %s:%u", s, __FUNCTION__, __LINE__); return r; } }while(0)
 
 #define LOG_IF_ERROR(s, ...)            do{ if (s) { LOG_ERROR_INTERNAL(__VA_ARGS__) } }while(0)
 #define LOG_IF_WARNING(s, ...)          do{ if (s) { LOG_WARNING_INTERNAL(__VA_ARGS__) } }while(0)
